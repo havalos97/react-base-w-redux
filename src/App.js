a768857e097ddcc4877b515aa1c51511fc1d3fad
Hector Avalos <gerardo.avalos97@hotmail.com>
2020-06-03 20:52:50 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<h1>Welcome to React.JS</h1>
+			</Fragment>
+		);
+	}
+}
+
+export default App;
