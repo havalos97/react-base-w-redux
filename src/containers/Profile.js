1eca631fda7883087c0121c06588a82d974c05b3
Hector Avalos <gerardo.avalos97@hotmail.com>
2020-06-06 12:13:46 -0500

    Handling multiple states (loginReducer and profileReducer) and multiple action creators (profileActions w/ mapDispatchToProps function).

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,16 +1,49 @@
-import React, {Component} from 'react';
+import React, {Fragment,Component} from 'react';
 import { connect } from 'react-redux';
+import Api from '../utils/Api';
+import * as profileActions from '../redux/actions/profileActions.js';
+import { bindActionCreators } from 'redux'
+import MaterialTable from 'material-table';
 
 class Profile extends Component {
+	componentDidMount() {
+		Api.loadMoviesData().then((movieList) => {
+			this.props.profileActions.setRows(movieList);
+		});
+	}
+
 	render() {
 		return (
-			<h1>Bienvenido {this.props.username}</h1>
+			<Fragment>
+				<h3>Bienvenido {this.props.loginState.username}</h3>
+				<MaterialTable
+					title="Countries"
+					columns={this.props.profileState.columns}
+					data={this.props.profileState.rows}
+				/>
+			</Fragment>
 		);
 	}
 }
 
 const mapStateToProps = (reducers) => {
-	return reducers.loginReducer;
+	const {profileReducer, loginReducer} = reducers;
+
+	const combinedStates = {
+		loginState: {
+			username: loginReducer.username,
+		},
+		profileState: {
+			...profileReducer,
+		},
+	}
+	return combinedStates;
 }
 
-export default connect(mapStateToProps)(Profile);
+function mapDispatchToProps(dispatch) {
+	return {
+		profileActions: bindActionCreators(profileActions, dispatch),
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
